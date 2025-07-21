7622e1028a61ba0d4b2ba543df5b7be79e5c7267
NexTR_Plue <p.plue1881@gmail.com>
2025-07-21 17:35:02 +0900

    feat: 이메일 중복 여부 검사 메서드 추가

--- admin/src/main/java/com/iroom/admin/repository/AdminRepository.java
+++ admin/src/main/java/com/iroom/admin/repository/AdminRepository.java
@@ -4,4 +4,5 @@ import com.iroom.admin.entity.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AdminRepository extends JpaRepository<Admin,Long> {
+    boolean existsByEmail(String email);
 }
