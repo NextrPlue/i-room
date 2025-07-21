b54167eed9737325034c695520b8f9993669fede
NexTR_Plue <p.plue1881@gmail.com>
2025-07-21 16:40:49 +0900

    feat: Admin repository 작성

---
+++ admin/src/main/java/com/iroom/admin/repository/AdminRepository.java
@@ -0,0 +1,7 @@
+package com.iroom.admin.repository;
+
+import com.iroom.admin.entity.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdminRepository extends JpaRepository<Admin,Long> {
+}
