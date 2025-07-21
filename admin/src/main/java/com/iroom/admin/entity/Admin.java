495f647f02b2330c26dd5f65c19a1143c30dfa6e
NexTR_Plue <p.plue1881@gmail.com>
2025-07-21 16:40:14 +0900

    feat: Admin 엔티티 작성

---
+++ admin/src/main/java/com/iroom/admin/entity/Admin.java
@@ -0,0 +1,46 @@
+package com.iroom.admin.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="Admin")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Admin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+    private String email;
+    private String password;
+    private String phone;
+    private String role;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    @PostPersist
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+
+    @Builder
+    public Admin(String email, String name, String password, String phone, String role) {
+        this.email = email;
+        this.name = name;
+        this.password = password;
+        this.phone = phone;
+        this.role = role;
+    }
+}
