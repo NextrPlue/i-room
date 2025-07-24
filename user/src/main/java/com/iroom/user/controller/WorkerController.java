6bec03c5be6f4a32dd70e229c341e17df6897a1f
NextrPlue <98487008+NextrPlue@users.noreply.github.com>
2025-07-24 21:47:16 +0900

    feat: 근로자 정보 변경 API 추가

--- user/src/main/java/com/iroom/user/controller/WorkerController.java
+++ user/src/main/java/com/iroom/user/controller/WorkerController.java
@@ -2,16 +2,15 @@ package com.iroom.user.controller;
 
 import com.iroom.user.dto.request.LoginRequest;
 import com.iroom.user.dto.request.WorkerRegisterRequest;
+import com.iroom.user.dto.request.WorkerUpdateInfoRequest;
 import com.iroom.user.dto.response.LoginResponse;
 import com.iroom.user.dto.response.WorkerRegisterResponse;
+import com.iroom.user.dto.response.WorkerUpdateResponse;
 import com.iroom.user.service.WorkerService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/workers")
@@ -29,6 +28,12 @@ public class WorkerController {
     @PostMapping("/login")
     public ResponseEntity<LoginResponse> login(@Valid @RequestBody LoginRequest request) {
         LoginResponse response = workerService.login(request);
+        return ResponseEntity.ok(response);
+    }
+
+    @PutMapping("/{workerId}")
+    public ResponseEntity<WorkerUpdateResponse> updateInfo(@PathVariable Long workerId, @RequestBody WorkerUpdateInfoRequest request) {
+        WorkerUpdateResponse response = workerService.updateWorkerInfo(workerId, request);
         return ResponseEntity.ok(response);
     }
 }
