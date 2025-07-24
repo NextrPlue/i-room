d0657d530cd4e266f2cc11da00ef59c9c1709f5a
NextrPlue <98487008+NextrPlue@users.noreply.github.com>
2025-07-24 21:02:55 +0900

    feat: 근로자 정보 이메일로 검색하는 메서드 추가

--- user/src/main/java/com/iroom/user/repository/WorkerRepository.java
+++ user/src/main/java/com/iroom/user/repository/WorkerRepository.java
@@ -3,6 +3,9 @@ package com.iroom.user.repository;
 import com.iroom.user.entity.Worker;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface WorkerRepository extends JpaRepository<Worker, Long> {
     boolean existsByEmail(String email);
+    Optional<Worker> findByEmail(String email);
 }
