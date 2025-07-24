e3ed6f073cc08f7fa83e23926459a7cf9c3ae79e
NextrPlue <98487008+NextrPlue@users.noreply.github.com>
2025-07-24 20:46:14 +0900

    feat: 근로자 등록 API 추가

---
+++ user/src/main/java/com/iroom/user/controller/WorkerController.java
@@ -0,0 +1,26 @@
+package com.iroom.user.controller;
+
+import com.iroom.user.dto.request.WorkerRegisterRequest;
+import com.iroom.user.dto.response.WorkerRegisterResponse;
+import com.iroom.user.service.WorkerService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/workers")
+@RequiredArgsConstructor
+public class WorkerController {
+
+    private final WorkerService workerService;
+
+    @PostMapping("/register")
+    public ResponseEntity<WorkerRegisterResponse> register(@Valid @RequestBody WorkerRegisterRequest request) {
+        WorkerRegisterResponse response = workerService.registerWorker(request);
+        return ResponseEntity.ok(response);
+    }
+}
