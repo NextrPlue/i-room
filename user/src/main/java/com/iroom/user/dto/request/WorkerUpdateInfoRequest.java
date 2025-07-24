57adbdde9a27492108ff5031171800712084f18d
NextrPlue <98487008+NextrPlue@users.noreply.github.com>
2025-07-24 21:46:39 +0900

    feat: 근로자 정보 변경 DTO 추가

---
+++ user/src/main/java/com/iroom/user/dto/request/WorkerUpdateInfoRequest.java
@@ -0,0 +1,33 @@
+package com.iroom.user.dto.request;
+
+import com.iroom.user.annotation.ValidPassword;
+import com.iroom.user.annotation.ValidPhone;
+import com.iroom.user.enums.WorkerRole;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record WorkerUpdateInfoRequest(
+        @NotBlank
+        @Size(min = 2, max = 20)
+        String name,
+
+        @NotBlank
+        @Email
+        String email,
+
+        @NotBlank
+        @ValidPhone
+        String phone,
+
+        String bloodType,
+        String gender,
+        Integer age,
+        Float weight,
+        Float height,
+        String jobTitle,
+        String occupation,
+        String department,
+        String faceImageUrl
+) {
+}
