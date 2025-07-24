86879caa2365487a1d1bc7e4ced89354af438ae5
NextrPlue <98487008+NextrPlue@users.noreply.github.com>
2025-07-24 22:21:00 +0900

    feat: 기본 CRUD API 비활성화

--- user/src/main/java/com/iroom/user/repository/AdminRepository.java
+++ user/src/main/java/com/iroom/user/repository/AdminRepository.java
@@ -2,9 +2,11 @@ package com.iroom.user.repository;
 
 import com.iroom.user.entity.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.Optional;
 
+@RepositoryRestResource(exported = false)
 public interface AdminRepository extends JpaRepository<Admin,Long> {
     boolean existsByEmail(String email);
     Optional<Admin> findByEmail(String email);
