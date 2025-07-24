adf81c3aed28b6440d025e1670fa91955ffb0474
NextrPlue <98487008+NextrPlue@users.noreply.github.com>
2025-07-24 21:46:58 +0900

    feat: 근로자 정보 변경 메서드 추가

--- user/src/main/java/com/iroom/user/service/WorkerService.java
+++ user/src/main/java/com/iroom/user/service/WorkerService.java
@@ -2,8 +2,10 @@ package com.iroom.user.service;
 
 import com.iroom.user.dto.request.LoginRequest;
 import com.iroom.user.dto.request.WorkerRegisterRequest;
+import com.iroom.user.dto.request.WorkerUpdateInfoRequest;
 import com.iroom.user.dto.response.LoginResponse;
 import com.iroom.user.dto.response.WorkerRegisterResponse;
+import com.iroom.user.dto.response.WorkerUpdateResponse;
 import com.iroom.user.entity.Worker;
 import com.iroom.user.jwt.JwtTokenProvider;
 import com.iroom.user.repository.WorkerRepository;
@@ -43,5 +45,19 @@ public class WorkerService {
         }
 
         return new LoginResponse(jwtTokenProvider.createWorkerToken(worker));
+    }
+
+    @PreAuthorize("hasAnyAuthority('ROLE_SUPER_ADMIN', 'ROLE_ADMIN')")
+    public WorkerUpdateResponse updateWorkerInfo(Long id, WorkerUpdateInfoRequest request) {
+        Worker worker = workerRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당하는 근로자를 찾을 수 없습니다."));
+
+        if (!worker.getEmail().equals(request.email()) && workerRepository.existsByEmail(request.email())) {
+            throw new IllegalArgumentException("이미 사용 중인 이메일입니다.");
+        }
+
+        worker.updateInfo(request);
+
+        return new WorkerUpdateResponse(worker);
     }
 }
