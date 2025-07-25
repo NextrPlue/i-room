3ef13e4f2ad25c9b46ca81df79c4887ec5015a57
NextrPlue <98487008+NextrPlue@users.noreply.github.com>
2025-07-25 13:17:57 +0900

    feat: 근로자 이름, 이메일 기반 검색 추가

--- user/src/main/java/com/iroom/user/repository/WorkerRepository.java
+++ user/src/main/java/com/iroom/user/repository/WorkerRepository.java
@@ -1,6 +1,8 @@
 package com.iroom.user.repository;
 
 import com.iroom.user.entity.Worker;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
@@ -10,4 +12,6 @@ import java.util.Optional;
 public interface WorkerRepository extends JpaRepository<Worker, Long> {
     boolean existsByEmail(String email);
     Optional<Worker> findByEmail(String email);
+    Page<Worker> findByNameContaining(String name, Pageable pageable);
+    Page<Worker> findByEmailContaining(String email, Pageable pageable);
 }
