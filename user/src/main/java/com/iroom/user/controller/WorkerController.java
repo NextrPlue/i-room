a91980c4dde0b5bef5f5365999efa4be6c0b6c24
NextrPlue <98487008+NextrPlue@users.noreply.github.com>
2025-07-25 13:18:30 +0900

    feat: 근로자 정보 조회 페이징 API 구현

--- user/src/main/java/com/iroom/user/controller/WorkerController.java
+++ user/src/main/java/com/iroom/user/controller/WorkerController.java
@@ -4,9 +4,7 @@ import com.iroom.user.dto.request.LoginRequest;
 import com.iroom.user.dto.request.WorkerRegisterRequest;
 import com.iroom.user.dto.request.WorkerUpdateInfoRequest;
 import com.iroom.user.dto.request.WorkerUpdatePasswordRequest;
-import com.iroom.user.dto.response.LoginResponse;
-import com.iroom.user.dto.response.WorkerRegisterResponse;
-import com.iroom.user.dto.response.WorkerUpdateResponse;
+import com.iroom.user.dto.response.*;
 import com.iroom.user.service.WorkerService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -42,5 +40,23 @@ public class WorkerController {
     public ResponseEntity<Void> updatePassword(@RequestHeader("X-User-Id") Long id, @Valid @RequestBody WorkerUpdatePasswordRequest request) {
         workerService.updateWorkerPassword(id, request);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<PagedResponse<WorkerInfoResponse>> getWorkers(
+            @RequestParam(required = false) String target,
+            @RequestParam(required = false) String keyword,
+            @RequestParam(defaultValue = "0") Integer page,
+            @RequestParam(defaultValue = "10") Integer size) {
+        if (size > 50) {
+            size = 50;
+        }
+
+        if (size < 0) {
+            size = 0;
+        }
+
+        PagedResponse<WorkerInfoResponse> response = workerService.getWorkers(target, keyword, page, size);
+        return ResponseEntity.ok(response);
     }
 }
