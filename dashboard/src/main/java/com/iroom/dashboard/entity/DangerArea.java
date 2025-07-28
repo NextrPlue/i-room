0b5791221f1d7fc149ab7b561e22dd2754b740fc
BaeSJ1 <bsj9278@gmail.com>
2025-07-28 11:39:50 +0900

    style: DangerArea 코드 스타일 정리

--- dashboard/src/main/java/com/iroom/dashboard/entity/DangerArea.java
+++ dashboard/src/main/java/com/iroom/dashboard/entity/DangerArea.java
@@ -8,33 +8,33 @@ import lombok.*;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "DangerArea")
 public class DangerArea {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    @Column(nullable = false)
-    private Long blueprintId;
+	@Column(nullable = false)
+	private Long blueprintId;
 
-    @Column(nullable = false)
-    private String location;
+	@Column(nullable = false)
+	private String location;
 
-    @Column(nullable = false)
-    private Double width;
+	@Column(nullable = false)
+	private Double width;
 
-    @Column(nullable = false)
-    private Double height;
+	@Column(nullable = false)
+	private Double height;
 
-    @Builder
-    public DangerArea (Long blueprintId, String location, Double width, Double height){
-        this.blueprintId = blueprintId;
-        this.location = location;
-        this.width = width;
-        this.height = height;
-    }
+	@Builder
+	public DangerArea(Long blueprintId, String location, Double width, Double height) {
+		this.blueprintId = blueprintId;
+		this.location = location;
+		this.width = width;
+		this.height = height;
+	}
 
-    public void update(String location, Double width, Double height){
-        this.location = location;
-        this.width = width;
-        this.height = height;
-    }
+	public void update(String location, Double width, Double height) {
+		this.location = location;
+		this.width = width;
+		this.height = height;
+	}
 }
