c7fdd693e48afc9345b99de5bb01761e01b91dfd
sorasol9 <gumza9go@gmail.com>
2025-07-28 11:38:36 +0900

    feat: workerId로 위치 조회 로직 테스트 코드 추가

--- sensor/src/test/java/com/iroom/sensor/service/WorkerHealthServiceTest.java
+++ sensor/src/test/java/com/iroom/sensor/service/WorkerHealthServiceTest.java
@@ -13,6 +13,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.iroom.sensor.dto.WorkerHealth.WorkerUpdateLocationRequest;
+import com.iroom.sensor.dto.WorkerHealth.WorkerUpdateLocationResponse;
 import com.iroom.sensor.dto.WorkerHealth.WorkerUpdateVitalSignsRequest;
 import com.iroom.sensor.entity.WorkerHealth;
 import com.iroom.sensor.repository.WorkerHealthRepository;
@@ -90,6 +91,25 @@ public class WorkerHealthServiceTest {
 		assertThatThrownBy(() -> workerService.updateVitalSigns(request))
 			.isInstanceOf(EntityNotFoundException.class)
 			.hasMessageContaining("해당 근로자 없음");
+	}
+
+	@Test
+	@DisplayName("workerId로 위치 조회 테스트")
+	void getWorkerLocationTest() {
+		Long workerId = 1L;
+		String location = "51.5072, 0.1275";
+
+		WorkerHealth workerHealth = WorkerHealth.builder().workerId(workerId).build();
+
+		workerHealth.updateLocation(location);
+
+		given(workerRepository.findByWorkerId(workerId)).willReturn(Optional.of(workerHealth));
+
+		WorkerUpdateLocationResponse response = workerService.getWorkerLocation(workerId);
+
+		assertThat(response.workerId()).isEqualTo(workerId);
+		assertThat(response.location()).isEqualTo(location);
+		verify(workerRepository).findByWorkerId(workerId);
 	}
 
 }
