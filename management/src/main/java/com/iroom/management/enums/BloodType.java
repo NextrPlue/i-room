dcbf8969171e375c32bcf21b28200c2c36ac0c2b
NexTR_Plue <p.plue1881@gmail.com>
2025-07-28 17:32:41 +0900

    feat: 근로자 정보 enum 추가

---
+++ management/src/main/java/com/iroom/management/enums/BloodType.java
@@ -0,0 +1,15 @@
+package com.iroom.management.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum BloodType {
+    A("A형"),
+    B("B형"),
+    AB("AB형"),
+    O("O형");
+
+    private final String description;
+}
