147d1612cff68d32d055200e5df6984b99df9537
KMinsu <minsue9608@naver.com>
2025-07-28 14:33:02 +0900

    feat: DeepSORT 기반 tracker 추가

--- safetyGear/camera_stream.py
+++ safetyGear/camera_stream.py
@@ -3,6 +3,7 @@ import cv2
 import time
 from safetyGear.utils import detect_and_draw
 from safetyGear.alert_service import send_alert
+from deep_sort_realtime.deepsort_tracker import DeepSort
 
 cap = cv2.VideoCapture(0)   # 웹캠
 
@@ -10,6 +11,13 @@ cap = cv2.VideoCapture(0)   # 웹캠
 REQUIRED_ITEMS = {"safety_harness_on", "safety_lanyard_on", "safety_helmet_on"}
 
 last_alert_time = 0 # 마지막 전송 시각
+
+# DeepSORT 설정
+tracker = DeepSort(
+    max_age = 30,   # 30프레임 동안 detection 없으면 track 삭제
+    n_init = 3,     # 3번 연속 detection 되어야 track 확정
+    max_iou_distance = 0.7
+)
 
 # 웹캠 프레임을 계속 읽어 스트리밍 + 알림 처리하는 제너레이터
 def generate_frames():
@@ -24,6 +32,29 @@ def generate_frames():
 
         # YOLO 탐지 실행 -> 객체 감지 후 Bounding Box를 그린 프레임과 감지 정보 반환
         frame, detections, detected_classes = detect_and_draw(frame)
+
+        # detections 리스트를 DeepSORT 입력 형식으로 변환
+        ds_detections = []
+        for det in detections:
+            x1, y1, x2, y2 = det['bbox']
+            w, h = x2 - x1, y2 - y1
+            ds_detections.append(([x1, y1, w, h],
+                                  det["confidence"],
+                                  det["class"]))
+            
+        # DeepSORT tracking
+        tracks = tracker.update_tracks(ds_detections, frame=frame)
+
+        # tracking 결과 화면에 표시
+        for t in tracks:
+            if not t.is_confirmed():
+                continue
+
+            x1, y1, x2, y2 = map(int, t.to_ltrb())
+            track_id = t.track_id
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 255), 2)
+            cv2.putText(frame, f"ID:{track_id}", (x1, y1 - 10),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 255), 2)
 
         # 미착용 여부 판단
         # 필수 보호구(required_items) - 실제 감지된 보호구(detected_classes)
