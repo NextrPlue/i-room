9c6298ed4573953435f191a0b66b6336f2bf7462
NextrPlue <98487008+NextrPlue@users.noreply.github.com>
2025-07-30 17:20:39 +0900

    feat: SystemAccountController 테스트 코드 작성

--- user/src/main/java/com/iroom/user/system/controller/SystemAccountController.java
+++ user/src/main/java/com/iroom/user/system/controller/SystemAccountController.java
@@ -3,6 +3,7 @@ package com.iroom.user.system.controller;
 import com.iroom.user.system.dto.request.SystemAuthRequest;
 import com.iroom.user.system.dto.response.SystemAuthResponse;
 import com.iroom.user.system.service.SystemAccountService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,7 +19,7 @@ public class SystemAccountController {
     private final SystemAccountService systemAccountService;
 
     @PostMapping("/authenticate")
-    public ResponseEntity<SystemAuthResponse> authenticate(@RequestBody SystemAuthRequest request) {
+    public ResponseEntity<SystemAuthResponse> authenticate(@RequestBody @Valid SystemAuthRequest request) {
         SystemAuthResponse response = systemAccountService.authenticate(request);
         return ResponseEntity.ok(response);
     }
