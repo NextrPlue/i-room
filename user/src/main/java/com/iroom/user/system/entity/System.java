ebd4a2eeeebb07d38fef844211e42b4f5b60900c
NextrPlue <98487008+NextrPlue@users.noreply.github.com>
2025-07-30 15:36:00 +0900

    fix: System entity 오타 수정

--- user/src/main/java/com/iroom/user/system/entity/System.java
+++ user/src/main/java/com/iroom/user/system/entity/System.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
 
 @Entity
-@Table("name=Systems")
+@Table(name="Systems")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class System {
