c980191de5b2d4873ff28f3e57128b46e849ec89
NextrPlue <98487008+NextrPlue@users.noreply.github.com>
2025-07-30 16:07:58 +0900

    feat: 미사용 메서드 삭제

--- user/src/main/java/com/iroom/user/system/repository/SystemRepository.java
+++ user/src/main/java/com/iroom/user/system/repository/SystemRepository.java
@@ -2,11 +2,11 @@ package com.iroom.user.system.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import com.iroom.user.system.entity.System;
 
 import java.util.Optional;
 
 @RepositoryRestResource(exported = false)
 public interface SystemRepository extends JpaRepository<System, Long> {
     Optional<System> findByApiKey(String apiKey);
-    Optional<System> findByName(String name);
 }
