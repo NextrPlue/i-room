ce119c5f52dab87873f2bea8c71590cf48ce916c
NextrPlue <98487008+NextrPlue@users.noreply.github.com>
2025-07-30 16:45:00 +0900

    feat: 역할 별로 SystemRole 추가

--- user/src/main/java/com/iroom/user/system/enums/SystemRole.java
+++ user/src/main/java/com/iroom/user/system/enums/SystemRole.java
@@ -6,7 +6,9 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum SystemRole {
-    SYSTEM("ROLE_SYSTEM");
+    ENTRANCE_SYSTEM("ROLE_ENTRANCE_SYSTEM"),
+    WORKER_SYSTEM("ROLE_WORKER_SYSTEM"),
+    EQUIPMENT_SYSTEM("ROLE_EQUIPMENT_SYSTEM");
 
     private final String key;
 }
