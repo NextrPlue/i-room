c306efcd160be5dd2e37687c3851c64be098b49e
NextrPlue <98487008+NextrPlue@users.noreply.github.com>
2025-07-30 14:19:30 +0900

    feat: CRUD API 자동 생성 방지

--- sensor/src/main/java/com/iroom/sensor/repository/HeavyEquipmentRepository.java
+++ sensor/src/main/java/com/iroom/sensor/repository/HeavyEquipmentRepository.java
@@ -3,6 +3,8 @@ package com.iroom.sensor.repository;
 import com.iroom.sensor.entity.HeavyEquipment;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(exported = false)
 public interface HeavyEquipmentRepository extends JpaRepository<HeavyEquipment, Long> {
 }
