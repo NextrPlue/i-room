f243b97e91ba8277f58d44b92861578fe4b52a75
KMinsu <minsue9608@naver.com>
2025-08-05 13:57:18 +0900

    fixt: input Feature 오류 수정

--- health_monitoring/utils/model_utils.py
+++ health_monitoring/utils/model_utils.py
@@ -6,7 +6,7 @@ import pandas as pd
 import os
 
 # 모델 경로 정의
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 MODEL_PATH = os.path.join(BASE_DIR, "models", "lgb_model_2.pkl")
 
 # 모델 로딩
@@ -16,10 +16,13 @@ with open(MODEL_PATH, "rb") as f:
 def predict_worker_risk(age, heart_rate):
     try:
         # 입력값 구성: 학습 시 사용한 컬럼명과 일치해야 함
+        hrmax = 220 - age if age is not None else 180
+        hrmax = max(hrmax, 1)
+        hr_ratio = heart_rate / hrmax
+
         input_df = pd.DataFrame([[
-            int(age),
-            float(heart_rate)
-        ]], columns=["age", "heartRate"])
+            int(age), float(heart_rate), float(hrmax), float(hr_ratio)
+        ]], columns=["Age", "HR", "HRmax", "hr_ratio"])
 
         # 예측 수행
         pred = model.predict(input_df)[0]
