0b9092bd2f1556e4aaf75f25a4b206f9ab306239
KMinsu <minsue9608@naver.com>
2025-08-05 10:47:36 +0900

    feat: KafkaConsumer 정의

--- health_monitoring/kafka_consumer.py
+++ health_monitoring/kafka_consumer.py
@@ -1,6 +1,15 @@
 # health_monitoring/kafka_consumer.py
-# 건강 이상 분류 요청 수신
+# 근로자 센서 이벤트 수신
 
 import json
 import threading
 from kafka import KafkaConsumer, KafkaProducer
+
+consumer = KafkaConsumer(
+    "iroom",
+    bootstrap_servers=["kafka:9093"],
+    auto_offset_reset="latest",
+    enable_auto_commit=True,
+    group_id="health-service",
+    value_deserializer=lambda m: json.loads(m.decode("utf-8"))
+)
