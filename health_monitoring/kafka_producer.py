f32599dec29de033a2a95e178239e671e1759f81
KMinsu <minsue9608@naver.com>
2025-08-05 13:23:05 +0900

    feat: 이벤트 발행 함수 추가

--- health_monitoring/kafka_producer.py
+++ health_monitoring/kafka_producer.py
@@ -8,3 +8,13 @@ producer = KafkaProducer(
     bootstrap_server=["localhost:9092"],
     value_serializer=lambda v: json.dumps(v).encode("utf-8")
 )
+
+def send_alert_event(worker_id, risk_level):
+    event = {
+        "eventType": "WORKER_HEALTH_ALERT",
+        "workerId": worker_id,
+        "riskLevel": risk_level # 0=정상, 1=위험
+    }
+
+    producer.send("worker_alerts", event)
+    print(f"예측 결과 전송 완료!: {event}")
