cd52216273cdef48481a6131c8e5eab695300a63
KMinsu <minsue9608@naver.com>
2025-08-05 13:16:21 +0900

    feat: 근로자 건강 이상 분류 결과 추가

--- health_monitoring/kafka_consumer.py
+++ health_monitoring/kafka_consumer.py
@@ -3,6 +3,7 @@
 
 import json
 from kafka import KafkaConsumer
+from utils.model_utils import predict_worker_risk
 
 # KafkaConsumer 정의
 consumer = KafkaConsumer(
@@ -17,7 +18,7 @@ consumer = KafkaConsumer(
 for message in consumer:
     data = message.value
     if data.get("eventType") == "WORKER_VITAL_SIGNS_UPDATED":
-        print(f"근로자 센서 데이터 수신: {data}")
+        print(f"근로자 센서 데이터 수신 성공!: {data}")
 
         """
         수신 메시지 예시
@@ -41,3 +42,16 @@ for message in consumer:
         # speed = data.get("speed")
         # pace = data.get("pace")
         # steps = data.get("steps")
+
+        worker_id = data.get("workerId")
+        age = data.get("age")
+        heart_rate = data.get("heartRate")
+
+        # 근로자 건강 이상 분류
+        print(f"{worker_id} 근로자 건강 이상 분류 시작!")
+        result = predict_worker_risk(age, heart_rate)
+
+        print(f"근로자 건강 이상 분류 완료!")
+        print(f"근로자 건강 상태: {'위험' if result == 1 else '정상'}")
+
+        
