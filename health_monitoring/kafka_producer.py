be17462870c24a21b2dce27bc3f745c2397412f9
KMinsu <minsue9608@naver.com>
2025-08-05 13:19:54 +0900

    feat: KafkaProducer 추가

--- health_monitoring/kafka_producer.py
+++ health_monitoring/kafka_producer.py
@@ -1,1 +1,10 @@
 # health_monitoring/kafka_producer.py
+# 근로자 건강 이상 분류 결과를 Kafka 토픽으로 발행
+
+from kafka import KafkaProducer
+import json
+
+producer = KafkaProducer(
+    bootstrap_server=["localhost:9092"],
+    value_serializer=lambda v: json.dumps(v).encode("utf-8")
+)
