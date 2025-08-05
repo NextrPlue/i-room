8eb4bfd36068f0423612732484c0b715e8618312
KMinsu <minsue9608@naver.com>
2025-08-05 13:25:52 +0900

    feat: 이벤트 발행 기능 추가

--- health_monitoring/kafka_consumer.py
+++ health_monitoring/kafka_consumer.py
@@ -4,6 +4,7 @@
 import json
 from kafka import KafkaConsumer
 from utils.model_utils import predict_worker_risk
+from kafka_producer import send_alert_event
 
 # KafkaConsumer 정의
 consumer = KafkaConsumer(
@@ -18,7 +19,7 @@ consumer = KafkaConsumer(
 for message in consumer:
     data = message.value
     if data.get("eventType") == "WORKER_VITAL_SIGNS_UPDATED":
-        print(f"근로자 센서 데이터 수신 성공!: {data}")
+        print(f"근로자 센서 데이터 수신 완료!: {data}")
 
         """
         수신 메시지 예시
@@ -47,11 +48,12 @@ for message in consumer:
         age = data.get("age")
         heart_rate = data.get("heartRate")
 
-        # 근로자 건강 이상 분류
-        print(f"{worker_id} 근로자 건강 이상 분류 시작!")
+        # 근로자 건강 이상 예측
+        print(f"{worker_id} 근로자 건강 이상 예측 시작!")
         result = predict_worker_risk(age, heart_rate)
 
-        print(f"근로자 건강 이상 분류 완료!")
+        print(f"근로자 건강 이상 예측 완료!")
         print(f"근로자 건강 상태: {'위험' if result == 1 else '정상'}")
 
-        
+        # 결과 이벤트 발행
+        send_alert_event(worker_id, result)
