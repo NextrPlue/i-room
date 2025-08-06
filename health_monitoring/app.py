66e16562a237cf0ed8245de4f979bfe6b8935fb1
KMinsu <minsue9608@naver.com>
2025-08-06 13:40:05 +0900

    feat: FastAPI 초기화 함수

--- health_monitoring/app.py
+++ health_monitoring/app.py
@@ -3,13 +3,27 @@
 
 from fastapi import FastAPI
 from pydantic import BaseModel
-
 import pandas as pd
 import os
 import joblib
+from utils.db import init_db, get_session
+from kafka_consumer import consume_worker_data
+from contextlib import asynccontextmanager
 
-# === FastAPI 초기화 ===
-app = FastAPI()
+app = FastAPI() # FastAPI 초기화
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("DB 초기화")
+    init_db()
+
+    print("Kafka Consumer 시작")
+    consume_worker_data()   # Kafka 소비 시작 (스레드로 작동)
+
+    yield   # 앱 실행 시작
+
+    # 종료 시 로직이 있다면 여기 작성
+    print("서버 종료 중..")
 
 # === 입력 스키마 정의 ===
 class HealthInput(BaseModel):
