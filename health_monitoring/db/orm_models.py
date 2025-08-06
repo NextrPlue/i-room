69ca6c77d573a228b3cfe5595a9362b0c91c1c4b
KMinsu <minsue9608@naver.com>
2025-08-06 10:34:33 +0900

    feat: incidents 테이블 정의

--- health_monitoring/db/orm_models.py
+++ health_monitoring/db/orm_models.py
@@ -1,1 +1,17 @@
 # /db/orm_models.py
+
+from sqlalchemy import Column, String, Float, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Incident(Base):
+    __tablename__ = "incidents"
+
+    IncidentId = Column(String, primary_key=True)
+    workerId = Column(String)
+    workerLatitude = Column(Float)
+    workerLongitude = Column(Float)
+    incidentType = Column(String)
+    incidentDescription = Column(String)
+    occurredAt = Column(DateTime)
