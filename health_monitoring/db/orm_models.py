363b47a5a84909b4ae54a80e5a92e492d7439f72
KMinsu <minsue9608@naver.com>
2025-08-06 14:09:38 +0900

    refactor: ID 생성 방식 변경

--- health_monitoring/db/orm_models.py
+++ health_monitoring/db/orm_models.py
@@ -1,6 +1,6 @@
 # /db/orm_models.py
 
-from sqlalchemy import Column, String, Float, DateTime
+from sqlalchemy import Column, Integer, String, Float, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 
 # 모든 ORM 모델의 부모 클래스
@@ -12,10 +12,10 @@ class Incident(Base):
     __tablename__ = "incidents" # DB에 incidents 테이블 생성
 
     # 컬럼 정의 -> DB 필드에 매핑
-    incidentId = Column(String, primary_key=True)   # 고유 위험 ID
-    workerId = Column(String)                       # 근로자 ID (kafka로 받음)
-    workerLatitude = Column(Float)                  # 근로자 위치 위도 (kafka로 받음)
-    workerLongitude = Column(Float)                 # 근로자 위치 경도 (kafka로 받음)
-    incidentType = Column(String)                   # 위험 정도 (정상 or 위험)
-    incidentDescription = Column(String)            # 상세 설명
-    occurredAt = Column(DateTime)                   # 위험 예측 시간
+    incidentId = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    workerId = Column(String)               # 근로자 ID (kafka로 받음)
+    workerLatitude = Column(Float)          # 근로자 위치 위도 (kafka로 받음)
+    workerLongitude = Column(Float)         # 근로자 위치 경도 (kafka로 받음)
+    incidentType = Column(String)           # 위험 정도 (정상 or 위험)
+    incidentDescription = Column(String)    # 상세 설명
+    occurredAt = Column(DateTime)           # 위험 예측 시간
