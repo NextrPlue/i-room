25d7c2a4a27a7eb85dc3426fd55444248e1c975c
KMinsu <minsue9608@naver.com>
2025-08-06 16:55:09 +0900

    fix: 의존성 관련 오류 수정

--- health_monitoring/app.py
+++ health_monitoring/app.py
@@ -1,16 +1,13 @@
 # health_monitoring/app.py
 # uvicorn health_monitoring.app:app --reload
 
-from fastapi import FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException
 from pydantic import BaseModel
-import pandas as pd
-import os
-import joblib
-from utils.db import init_db, get_session
-from kafka_consumer import consume_worker_data
+from health_monitoring.utils.db import init_db, get_session
+from health_monitoring.kafka_consumer import consume_worker_data
 from contextlib import asynccontextmanager
-from utils.schemas import IncidentResponse
-from db.orm_models import Incident
+from health_monitoring.utils.schemas import IncidentResponse
+from health_monitoring.db.orm_models import Incident
 from sqlalchemy.orm import Session
 
 @asynccontextmanager
@@ -33,7 +30,7 @@ def root():
     return {"message": "근로자 건강 이상 예측 시스템 실행 중!"}
 
 @app.get("/incident/{incident_id}", response_model=IncidentResponse)
-def get_incident_by_id(incident_id: str, db: Session = get_session()):
+def get_incident_by_id(incident_id: int, db: Session = Depends(get_session)):
     incident = db.query(Incident).filter(Incident.incidentId == incident_id).first()
     if not incident:
         raise HTTPException(status_code=404, detail="해당 incidentId를 찾을 수 없습니다.")
