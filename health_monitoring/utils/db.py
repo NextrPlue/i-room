d00783acba493d8b4f09d0eda99caed55503786b
KMinsu <minsue9608@naver.com>
2025-08-06 10:57:50 +0900

    feat: DB_URL 설정

--- health_monitoring/utils/db.py
+++ health_monitoring/utils/db.py
@@ -1,1 +1,14 @@
 # /utils/db.py
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, Session
+from db.orm_models import Base
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_URL = (
+    f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASS')}"
+    f"@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
+)
