d9ccc8918712300fe8bf721d08419d7708eb4dd2
KMinsu <minsue9608@naver.com>
2025-08-06 13:56:25 +0900

    feat: 조회용 엔드포인트 추가

--- health_monitoring/app.py
+++ health_monitoring/app.py
@@ -1,7 +1,7 @@
 # health_monitoring/app.py
 # uvicorn health_monitoring.app:app --reload
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import pandas as pd
 import os
@@ -9,6 +9,9 @@ import joblib
 from utils.db import init_db, get_session
 from kafka_consumer import consume_worker_data
 from contextlib import asynccontextmanager
+from utils.schemas import IncidentResponse
+from db.orm_models import Incident
+from sqlalchemy.orm import Session
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -24,3 +27,14 @@ async def lifespan(app: FastAPI):
     print("서버 종료 중..")
 
 app = FastAPI(lifespan=lifespan) # FastAPI 초기화, lifespan 방식
+
+@app.get("/")
+def root():
+    return {"message": "근로자 건강 이상 예측 시스템 실행 중!"}
+
+@app.get("/incident/{incident_id}", response_model=IncidentResponse)
+def get_incident_by_id(incident_id: str, db: Session = get_session()):
+    incident = db.query(Incident).filter(Incident.incidentId == incident_id).first()
+    if not incident:
+        raise HTTPException(status_code=404, detail="해당 incidentId를 찾을 수 없습니다.")
+    return incident
