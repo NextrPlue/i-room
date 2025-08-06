b955c78d176de3ea33b41dd30c70390d6f035885
KMinsu <minsue9608@naver.com>
2025-08-06 11:31:37 +0900

    feat: Pydantic 응답 모델 정의

--- health_monitoring/utils/schemas.py
+++ health_monitoring/utils/schemas.py
@@ -1,1 +1,16 @@
 # /utils/schemas.py
+
+from pydantic import BaseModel
+from datetime import datetime
+
+class IncidentResponse(BaseModel):
+    incidentId: str
+    workerId: str
+    workerLatitude: float
+    workerLongitude: float
+    incidentType: str
+    incidentDescription: str
+    occurredAt: datetime
+
+    class Config:
+        orm_mode = True
