d3292bc69b8e539cde1dd86be1f3998033c8a333
KMinsu <minsue9608@naver.com>
2025-08-06 13:22:27 +0900

    feat: 백그라운드 Kafka 통신 기능

--- health_monitoring/kafka_consumer.py
+++ health_monitoring/kafka_consumer.py
@@ -9,6 +9,8 @@ from db.orm_models import Incident
 from sqlalchemy.orm import Session
 import uuid
 from datetime import datetime
+from utils.db import SessionLocal
+import threading
 
 # Kafka 메시지를 받아 건강 이상 여부를 판단하고 DB에 기록 및 결과 발행 함수
 def process_message(data: dict, db: Session):
@@ -56,6 +58,32 @@ def process_message(data: dict, db: Session):
 
     # 결과 Kafka 전송
     send_alert_event(worker_id, latitude, longitude, result)
+
+def consume_worker_data():
+    def run():
+        # KafkaConsumer 정의
+        consumer = KafkaConsumer(
+            "iroom",                                # 센서 이벤트가 발행되는 토픽
+            bootstrap_servers=['localhost:9092'],   # Kafka 브로커 주소
+            auto_offset_reset="latest",             # 최신 메시지부터 소비
+            enable_auto_commit=True,                
+            group_id="health-service"               # Cousumer 그룹 ID
+        )
+
+        db = SessionLocal()
+        print("Kafka 소비 시작")
+        for message in consumer:
+            try:
+                raw = message.value.decode("utf-8") # JSON 파싱
+                data = json.loads(raw)  # JSON 변환
+                process_message(data, db)
+            except Exception as e:
+                print("Kafka 메시지 처리 중 오류:", e)
+
+    # 백그라운드 스레드 실행
+    # FastAPI가 실행되면 자동으로 Kafka를 소비하기 시작
+    thread = threading.Thread(target=run, daemon=True)
+    thread.start()
 
 print("\n\n***실시간 근로자 건강 이상 예측 AI 서비스 시작***\n\n")
 
