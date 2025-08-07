32e9bf805457f36d9554d47fa7be97a66e26f816
kalina01255@naver.com <kalina01255@naver.com>
2025-08-07 13:02:20 +0900

    refactor: watch orm 수정

--- ppe_service/orm/watch.py
+++ ppe_service/orm/watch.py
@@ -6,7 +6,9 @@ class Watch(Base):
     __tablename__ = "watch"
 
     id = Column(Integer, primary_key=True, index=True)
-    workerId = Column(Integer, nullable=False)
+    worker_id = Column(Integer, nullable=False)
     latitude = Column(Float, nullable=False)
     longitude = Column(Float, nullable=False)
     timestamp = Column(DateTime, default=datetime.datetime.utcnow)
+
+
