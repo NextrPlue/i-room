46fd315e0eceec6899ce5c2fc7e1cf88b81d72ba
kalina01255@naver.com <kalina01255@naver.com>
2025-08-07 13:02:00 +0900

    refactor : ppe orm 수정

--- ppe_service/orm/ppe.py
+++ ppe_service/orm/ppe.py
@@ -2,11 +2,11 @@ from sqlalchemy import Column, BigInteger, Integer, String, Text, DateTime
 from database import Base
 import datetime
 
-class PPE(Base):
+class PPEDetection(Base):
     __tablename__ = "ppe"
 
     id = Column(BigInteger, primary_key=True, autoincrement=True)
-    ppe_id = Column(BigInteger, unique=True, nullable=False)
+    ppe_id = Column(BigInteger, unique=True, nullable=True)
     image_url = Column(Text)
     timestamp = Column(DateTime, default=datetime.datetime.utcnow, nullable=False)
     helmet_on_count = Column(Integer, default=0)
