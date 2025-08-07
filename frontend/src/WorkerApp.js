606144d04cfe0dae9f6033f8355308e30128f0e2
sorasol9 <gumza9go@gmail.com>
2025-08-07 17:32:53 +0900

    feat: 근로자 페이지 독립 구현을 위한 WorkerApp.js 생성

---
+++ frontend/src/WorkerApp.js
@@ -0,0 +1,55 @@
+// 근로자 레이아웃 컴포넌트
+const WorkerLayout = ({ children }) => {
+    return (
+        <div className="worker-app-container">
+            <div className="worker-layout">
+                {children}
+            </div>
+        </div>
+    );
+};
+
+const WorkerApp = () => {
+    const WorkerLoginPage = () => {
+        const navigate = useNavigate();
+
+        const handleLogin = (loginData) => {
+            // 로그인 처리 로직
+            console.log('근로자 로그인:', loginData);
+            navigate('/home');
+        };
+
+        return <WorkerLogin onLogin={handleLogin}/>;
+    };
+
+    return (
+        <Router>
+            <Routes>
+                {/* 기본 경로는 로그인으로 */}
+                <Route path="/" element={<Navigate to="/login"/>}/>
+                <Route path="/login" element={<WorkerLoginPage/>}/>
+
+                {/* 로그인 후 페이지들 */}
+                <Route path="/home" element={
+                    <WorkerLayout>
+                        <WorkerHome/>
+                    </WorkerLayout>
+                }/>
+
+                <Route path="/safety" element={
+                    <WorkerLayout>
+                        <WorkerSafety/>
+                    </WorkerLayout>
+                }/>
+
+                <Route path="/schedule" element={
+                    <WorkerLayout>
+                        <WorkerSchedule/>
+                    </WorkerLayout>
+                }/>
+            </Routes>
+        </Router>
+    );
+};
+
+export default WorkerApp;
