fc08cfb1fbbbe49719cbd28194b347a1bb0c472e
NextrPlue <98487008+NextrPlue@users.noreply.github.com>
2025-08-07 11:02:41 +0900

    feat: WorkerManagement PK 생성 전략 변경

--- management/src/main/java/com/iroom/management/entity/WorkerManagement.java
+++ management/src/main/java/com/iroom/management/entity/WorkerManagement.java
@@ -20,7 +20,7 @@ import lombok.NoArgsConstructor;
 @Table(name = "WorkerManagement")
 public class WorkerManagement {
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	@Column(nullable = false)
