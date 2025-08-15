89356c87240af44dd3ddce2e2699532e33c9bd76
NextrPlue <98487008+NextrPlue@users.noreply.github.com>
2025-08-15 22:41:23 +0900

    feat: 다중 근로자 센서 정보 조회 추가

--- sensor/src/main/java/com/iroom/sensor/repository/WorkerSensorRepository.java
+++ sensor/src/main/java/com/iroom/sensor/repository/WorkerSensorRepository.java
@@ -5,9 +5,12 @@ import com.iroom.sensor.entity.WorkerSensor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
 import java.util.Optional;
 
 @RepositoryRestResource(exported = false)
 public interface WorkerSensorRepository extends JpaRepository<WorkerSensor, Long> {
 	Optional<WorkerSensor> findByWorkerId(Long workerId);
+	
+	List<WorkerSensor> findByWorkerIdIn(List<Long> workerIds);
 }
